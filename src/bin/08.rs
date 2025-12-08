3caddeefd634005bf719f0b0967ebdf420675965
Chris Parks <christopher.daniel.parks@gmail.com>
2025-12-08 10:00:57 -0800

    Use disjoint set data structure for day 8
    Faster in release mode, but not much difference in debug

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,7 +1,6 @@
-use std::collections::{HashMap, HashSet};
-use std::rc::Rc;
-
 use advent_of_code::point::Point3;
+use disjoint::DisjointSet;
+use itertools::Itertools;
 use sscanf::scanf;
 
 advent_of_code::solution!(8);
@@ -11,65 +10,49 @@ pub fn part_one(input: &str) -> Option<u64> {
 
     // example only uses 10 pairs for some reason
     let wanted = if points.len() < 1000 { 10 } else { 1000 };
-
-    let mut pairs: Vec<(Point, Point)> = points
-        .iter()
-        .enumerate()
-        .flat_map(|(i, p)| points.iter().skip(i + 1).map(|q| (*p, *q)))
+    let pairs: Vec<(usize, usize)> = (0..points.len())
+        .cartesian_product(0..points.len())
+        .filter(|(i, j)| i < j)
+        .sorted_by_key(|(i, j)| points[*i].dist_squared(points[*j]))
+        .take(wanted)
         .collect();
 
-    pairs.sort_by_key(|(p, q)| p.dist_squared(*q));
+    let mut circuits = DisjointSet::with_len(points.len());
 
-    // Use reference-counted circuits to avoid a lot of cloning
-    let mut circuits: HashMap<Point, Rc<HashSet<Point>>> = points
-        .into_iter()
-        .map(|p| (p, Rc::new(HashSet::from_iter(vec![p]))))
-        .collect();
-
-    for (p, q) in pairs.into_iter().take(wanted) {
-        let merged: Rc<HashSet<Point>> =
-            Rc::new(circuits[&p].union(&circuits[&q]).copied().collect());
-        for u in merged.iter() {
-            circuits.insert(*u, merged.clone());
+    for (i, j) in pairs.into_iter() {
+        if !circuits.is_joined(i, j) {
+            circuits.join(i, j);
         }
     }
 
-    let mut sizes: Vec<usize> = circuits
-        .into_values()
-        .flat_map(|circuit| Rc::into_inner(circuit)) // unique via refcount
-        .map(|circuit| circuit.len())
-        .collect();
-
-    sizes.sort();
-
-    Some(sizes.into_iter().rev().take(3).product::<usize>() as u64)
+    Some(
+        circuits
+            .sets()
+            .into_iter()
+            .map(|circuit| circuit.len())
+            .k_largest(3)
+            .product::<usize>() as u64,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let points = parse(input);
-    let wanted = points.len();
-
-    let mut pairs: Vec<(Point, Point)> = points
-        .iter()
-        .enumerate()
-        .flat_map(|(i, p)| points.iter().skip(i + 1).map(|q| (*p, *q)))
+    let pairs: Vec<(usize, usize)> = (0..points.len())
+        .cartesian_product(0..points.len())
+        .filter(|(i, j)| i < j)
+        .sorted_by_key(|(i, j)| points[*i].dist_squared(points[*j]))
         .collect();
 
-    pairs.sort_by_key(|(p, q)| p.dist_squared(*q));
+    let mut circuits = DisjointSet::with_len(points.len());
+    let mut num_circuits = points.len();
 
-    let mut circuits: HashMap<Point, Rc<HashSet<Point>>> = points
-        .into_iter()
-        .map(|p| (p, Rc::new(HashSet::from_iter(vec![p]))))
-        .collect();
-
-    for (p, q) in pairs.into_iter() {
-        let merged: Rc<HashSet<Point>> =
-            Rc::new(circuits[&p].union(&circuits[&q]).copied().collect());
-        for u in merged.iter() {
-            circuits.insert(*u, merged.clone());
-        }
-        if merged.len() >= wanted {
-            return Some((p.x * q.x) as u64);
+    for (i, j) in pairs.into_iter() {
+        if !circuits.is_joined(i, j) {
+            circuits.join(i, j);
+            num_circuits -= 1;
+            if num_circuits <= 1 {
+                return Some((points[i].x * points[j].x) as u64);
+            }
         }
     }
     None
